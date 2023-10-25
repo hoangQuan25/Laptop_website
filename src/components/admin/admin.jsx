e5699aa4b18b0dc2ba0bbdd4eb16bdfb0c8dbea2
hoangQuan25 <quan.nh215127@sis.hust.edu.vn>
2023-10-25 23:41:58 +0700

    modify admin site

--- src/components/admin/admin.jsx
+++ src/components/admin/admin.jsx
@@ -1,22 +1,122 @@
-import React from 'react'
-import './admin.css'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { AiOutlineShoppingCart, AiOutlineUserAdd } from 'react-icons/ai'
+import { BsCoin } from 'react-icons/bs'
+import './admin.css';
 
-const DashboardBox = ({ title, value }) => {
+const DashboardBox = ({ title, value, icon }) => {
   return (
     <div className="dashboard-box">
-      <h4>{title}</h4>
-      <p>{value}</p>
+      <div className='icon'>{icon}</div>
+      <div className='content'>
+        <h2>{value}</h2>
+        <h4>{title}</h4>
+      </div>
     </div>
   );
 };
 
 const Admin = () => {
+  const [products, setProducts] = useState(0);
+  const [customers, setCustomers] = useState(0);
+  const [revenue, setRevenue] = useState(0);
+
+  useEffect(() => {
+    // Fetch data from the server
+    axios.get('/admin')
+      .then(response => {
+        console.log(response.data);
+        setProducts(response.data.num_of_products);
+        setCustomers(response.data.total_users);
+        setRevenue(response.data.total_revenue);
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }, []); // Empty dependency array ensures the effect runs only once
+
+  // handle the add product
+  const [product, setProduct] = useState(
+    {
+      Title: '',
+      Cat: '',
+      Brand: '',
+      Price: '',
+      Img: '',
+      Describe: ''
+    }
+  );
+
+  let name, value;
+  const data = (e) => {
+    name = e.target.name;
+    value = e.target.value;
+    setProduct({ ...product, [name]: value })
+  };
+
+  const sendData = async (e) => {
+    e.preventDefault();
+
+    // Check for empty fields
+    if (!product.Title || !product.Cat || !product.Brand || !product.Price || !product.Img || !product.Describe) {
+      alert('Please fill in all fields');
+      return;
+    }
+
+    // If all checks pass, proceed with sending data
+    const { Title, Cat, Brand, Price, Img, Describe } = product;
+    const options = {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify({
+        Title, Cat, Brand, Price, Img, Describe
+      })
+    };
+
+    try {
+      const res = await fetch('http://localhost:3000/admin', options);
+
+      if (res.ok) {
+        alert('Product added successfully');
+      } else {
+        alert('An error has occurred');
+      }
+    } catch (error) {
+      console.error('Error sending data:', error);
+      alert('An error has occurred');
+    }
+  };
+
   return (
-    <div className="admin-dashboard">
-      <DashboardBox title="Products" value="16 Products" />
-      <DashboardBox title="Customers" value="12 Customers" />
-      <DashboardBox title="Revenue" value="215,000,000 VND" />
-    </div>
+    <>
+      <div className="upper">
+        <div className="admin-dashboard">
+          <DashboardBox title="Products" value={products} icon={<AiOutlineShoppingCart />} />
+          <DashboardBox title="Customers" value={customers} icon={<AiOutlineUserAdd />} />
+          <DashboardBox title="Revenue this month" value={revenue + ' Đ'} icon={<BsCoin />} />
+        </div>
+      </div>
+      <div className="lower">
+        <div className="form-section">
+          <h2>Add product</h2>
+          <form method='POST'>
+            <input type='text' name='Title' value={product.Title} placeholder='Title' required autoComplete='off' onChange={data} />
+            <input type='text' name='Cat' value={product.Cat} placeholder='Cat' required autoComplete='off' onChange={data} />
+            <input type='text' name='Brand' value={product.Brand} placeholder='Brand' required autoComplete='off' onChange={data} />
+            <input type='text' name='Price' value={product.Price} placeholder='Price' required autoComplete='off' onChange={data} />
+            <input type='text' name='Img' value={product.Img} placeholder='Img link' required autoComplete='off' onChange={data} />
+            <textarea name='Describe' value={product.Describe} placeholder='Describe' required autoComplete='off' onChange={data} />
+            <button type='submit' onClick={sendData}>Add product</button>
+          </form>
+        </div>
+        <div className="messages-section">
+          <h2>Recent messages</h2>
+          <div className="content"></div>
+        </div>
+      </div>
+    </>
   );
 };
 
--- src/server/server.js
+++ src/server/server.js
@@ -8,152 +8,153 @@ const port = 3001;
 // Middleware to parse JSON data
 app.use(bodyParser.json());
 
-// Function to log messages to a file
-function logMessage(message) {
-    const timestamp = new Date().toISOString();
-    const logEntry = `${timestamp} - ${message}\n\n`;
-
-    fs.appendFile('Contact.log', logEntry, (err) => {
-        if (err) {
-            console.error('Error writing to log file:', err);
-        }
-    });
-}
-
-function logChange(message) {
-    const timestamp = new Date().toISOString();
-    const logEntry = `${timestamp} - ${message}\n\n`;
-
-    fs.appendFile('Update.log', logEntry, (err) => {
-        if (err) {
-            console.error('Error writing to log file:', err);
-        }
-    });
-}
-
-// Route to handle incoming messages
-app.post('/messages', (req, res) => {
+// add messages to server
+app.post('/messages', async (req, res) => {
+  try {
     const { Name, Email, Subject, Message } = req.body;
 
-    // Log the message
-    logMessage(`Received message from :\n\t${Name} (${Email}): \n\t${Subject} \n\t${Message}`);
+    // Insert the message into the "contact" table with the current date
+    const result = await pool.query(
+      'INSERT INTO contact (name, email, subject, message, date) VALUES ($1, $2, $3, $4, NOW())',
+      [Name, Email, Subject, Message]
+    );
 
     // Send a response back to the client
     res.json({ success: true, message: 'Message received successfully' });
+  } catch (error) {
+    console.error('Error inserting message into database:', error);
+    res.status(500).json({ success: false, message: 'Internal Server Error' });
+  }
 });
 
 
 // connect to database
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'laptop_website',
-    password: 'sinhvien',
-    port: 5432, // Change the port if needed
+  user: 'postgres',
+  host: 'localhost',
+  database: 'laptop_website',
+  password: 'sinhvien',
+  port: 5432, // Change the port if needed
 });
 
 // Fetch data from the laptops table
 async function fetchLaptopsData() {
-    const client = await pool.connect();
+  const client = await pool.connect();
 
-    try {
-        const result = await client.query('SELECT * FROM laptops');
-        return result.rows;
-    } finally {
-        client.release();
-    }
+  try {
+    const result = await client.query('SELECT * FROM laptops');
+    return result.rows;
+  } finally {
+    client.release();
+  }
 }
 
 // send bill data to the db
 app.post('/cart', async (req, res) => {
-    try {
-      const { email, cart, totalPrice } = req.body;
-  
-      // Insert into 'bill' table
-      const billResult = await pool.query(
-        'INSERT INTO bill (date, email, totalprice) VALUES (NOW(), $1, $2) RETURNING id',
-        [email, totalPrice]
-      );
-  
-      const billId = billResult.rows[0].id;
-  
-      // Insert into 'bill_detail' table for each product in the cart
-      for (const cartItem of cart) {
-        await pool.query('INSERT INTO bill_detail (bill_id, product_id, quantity) VALUES ($1, $2, $3)', [billId, cartItem.id, cartItem.qty]);
-      }
-  
-      res.status(201).json({ message: 'Bill and details added successfully', billId });
-    } catch (error) {
-      console.error('Error adding bill and details:', error);
-      res.status(500).json({ error: 'Internal Server Error' });
+  try {
+    const { email, cart, totalPrice } = req.body;
+
+    // Insert into 'bill' table
+    const billResult = await pool.query(
+      'INSERT INTO bill (date, email, totalprice) VALUES (NOW(), $1, $2) RETURNING id',
+      [email, totalPrice]
+    );
+
+    const billId = billResult.rows[0].id;
+
+    // Insert into 'bill_detail' table for each product in the cart
+    for (const cartItem of cart) {
+      await pool.query('INSERT INTO bill_detail (bill_id, product_id, quantity) VALUES ($1, $2, $3)', [billId, cartItem.id, cartItem.qty]);
     }
-  });
+
+    res.status(201).json({ message: 'Bill and details added successfully', billId });
+  } catch (error) {
+    console.error('Error adding bill and details:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
 
 
 // add user to the server
 app.post('/', async (req, res) => {
-    try {
-      const { name } = req.body;
-  
-      // Check if the email already exists
-      const checkQuery = 'SELECT * FROM users WHERE user_email = $1';
-      const checkValues = [name];
-      const existingUser = await pool.query(checkQuery, checkValues);
-  
-      if (existingUser.rows.length === 0) {
-        // Only insert the user if the email does not exist
-        // Assuming 'users' is your PostgreSQL table name
-        const insertQuery = 'INSERT INTO users (user_id, user_email) VALUES (DEFAULT, $1) RETURNING *';
-        const insertValues = [name];
-  
-        const result = await pool.query(insertQuery, insertValues);
-  
-        console.log('User added to the database:', result.rows[0]);
-        res.status(201).json({ message: 'User added successfully' });
-      } else {
-        // Do nothing or send a different response if needed
-        res.status(200).json({ message: 'User already exists, did nothing' });
-      }
-    } catch (error) {
-      console.error('Error handling request:', error);
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
-  });
+  try {
+    const { name } = req.body;
 
+    // Check if the email already exists
+    const checkQuery = 'SELECT * FROM users WHERE user_email = $1';
+    const checkValues = [name];
+    const existingUser = await pool.query(checkQuery, checkValues);
+
+    if (existingUser.rows.length === 0) {
+      // Only insert the user if the email does not exist
+      // Assuming 'users' is your PostgreSQL table name
+      const insertQuery = 'INSERT INTO users (user_id, user_email) VALUES (DEFAULT, $1) RETURNING *';
+      const insertValues = [name];
+
+      const result = await pool.query(insertQuery, insertValues);
+
+      console.log('User added to the database:', result.rows[0]);
+      res.status(201).json({ message: 'User added successfully' });
+    } else {
+      // Do nothing or send a different response if needed
+      res.status(200).json({ message: 'User already exists, did nothing' });
+    }
+  } catch (error) {
+    console.error('Error handling request:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+// get stats from database
+app.get('/admin', async (req, res) => {
+  try {
+    // Query for total users
+    const usersResult = await pool.query('SELECT COUNT(*) AS total_users FROM users');
+
+    // Query for total revenue of the month
+    const revenueResult = await pool.query(`
+      SELECT SUM(totalprice) AS total_revenue
+      FROM bill
+      WHERE EXTRACT(YEAR FROM date) = EXTRACT(YEAR FROM CURRENT_DATE)
+        AND EXTRACT(MONTH FROM date) = EXTRACT(MONTH FROM CURRENT_DATE)
+    `);
+
+    // Query for the number of products in laptops
+    const productsResult = await pool.query('SELECT COUNT(*) AS num_of_products FROM laptops');
+
+    // Send the results as JSON
+    res.json({
+      total_users: usersResult.rows[0].total_users,
+      total_revenue: revenueResult.rows[0].total_revenue || 0, // Ensure a default value if there is no revenue
+      num_of_products: productsResult.rows[0].num_of_products,
+    });
+  } catch (error) {
+    console.error('Error fetching stats:', error);
+    res.status(500).json({ success: false, message: 'Internal Server Error' });
+  }
+});
 // Format the fetched data into the structure used in productdetail.js
 async function formatDataForProductDetail() {
-    const laptopsData = await fetchLaptopsData();
+  const laptopsData = await fetchLaptopsData();
 
-    const formattedData = laptopsData.map(laptop => ({
-        id: laptop.id,
-        Title: laptop.title,
-        Cat: laptop.category,
-        Brand: laptop.brand,
-        Price: laptop.price,
-        Img: laptop.image_url,
-        Describe: laptop.description,
-    }));
+  const formattedData = laptopsData.map(laptop => ({
+    id: laptop.id,
+    Title: laptop.title,
+    Cat: laptop.category,
+    Brand: laptop.brand,
+    Price: laptop.price,
+    Img: laptop.image_url,
+    Describe: laptop.description,
+  }));
 
-    // Export the formatted data to a file (productdetail.js)
-    fs.writeFileSync('../components/data/productdetail.js', `const Productdetail = ${JSON.stringify(formattedData, null, 2)};\n\nexport default Productdetail;`);
-
-    // log the changes into a file
-    logChange(`Modified product list.\n\n`);
-
+  // Export the formatted data to a file (productdetail.js)
+  fs.writeFileSync('../components/data/productdetail.js', `const Productdetail = ${JSON.stringify(formattedData, null, 2)};\n\nexport default Productdetail;`);
 }
 
 // Call the function to format the data and export to productdetail.js
 formatDataForProductDetail();
-// then(() => {
-//     pool.end();
-// }).catch(error => {
-//     console.error('Error:', error);
-//     pool.end();
-// });
-
-
 
 // Start the server
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
